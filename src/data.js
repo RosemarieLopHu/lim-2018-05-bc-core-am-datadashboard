8c03001bc0351fc66ddf9c92b7372042810c9f12
RosemarieLopezH <rosemarie61@gmail.com>
2018-06-21 08:13:27 -0500

    cambios en data

--- src/data.js
+++ src/data.js
@@ -1,8 +1,23 @@
 window.computeUsersStats = (users, progress, courses) => {
-  console.log(users);
-  console.log(progress);
-  console.log(courses);
-}
-
-window.sortUsers = (users, orderBy, orderDirection) => {users, orderBy, orderDirection}
-window.filterUsers = (users, search) => {users, search}
+// funciones a exportar en el entorno global window
+  const arrayProgress = Object.keys(progress);
+  for (const students of users ){
+    arrayProgress.map((idUsers)=>{
+     if(students.id===idUsers){
+       if(progress[idUsers].intro !== undefined) {
+         let userWithStats =  {
+           stat : {
+             percent: progress[idUsers].intro.percent,
+           }
+         };
+         console.log(userWithStats);
+       }
+     }
+    })
+  }
+};
+//Función para ordenar lista de usuarios
+window.sortUsers = (users, orderBy, orderDirection) => {
+};
+window.filterUsers = (users, search) => {
+};
--- src/main.js
+++ src/main.js
@@ -1,29 +1,29 @@
-const Userbtn = document.getElementById('btn-menu');
-const listSedes = document.getElementById('response-container');
-const listUsers = document.getElementById('search-student');
+const Userbtn = document.getElementById('btn-users');
+//const Userbtn = document.getElementById('btn-menu');
+//const listSedes = document.getElementById('response-container');
+//const listUsers = document.getElementById('search-student');
 const urlC = '../data/cohorts.json';
 const urlP = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
 const urlU = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
 
-const getJson = (url,callback) => {
-    const request = new XMLHttpRequest();
-    request.open('GET', url);
-    request.onload = callback;
-    request.onerror = handleError;
-    request.send();
+const getJson = (url, callback) => {
+  const request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.onload = callback;
+  request.onerror = handleError;
+  request.send();
 }
 const handleError = () => {
-    console.log('Se ha presentado un error');
+  console.log('Se ha presentado un error');
 }
-// Progreso de lista de usuarios
-const getProgress = (users, courses) => {
-    let progress = [];
-    getJson(urlP,(err,progressjson) => {
-        for (const key in progressjson) {
-            if (progressjson[key].intro) {
-                progress.push(progressjson);
-            }
-        }
-    });
-    computeUsersStats(users, progress, courses);
+// definiendo el parseo de variables
+
+const progresoUsuario = () => {
+  const usuario = JSON.parse(event.target.responseText);
+  const progress = (event) => {
+    const data2 = JSON.parse(event.target.responseText);
+    computeUsersStats(usuario, data2);
+  }
+  getJson(urlP, progress);
 }
+getJson(urlU,(progresoUsuario))
